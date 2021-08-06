c80c09930a9e73fc833b49ef34e2dc0941d64e87
zacharykaszycki <zacharykaszycki@gmail.com>
2021-08-06 16:28:27 -0500

    A early version of login works

--- UserMicroservice/src/main/java/rev/controller/LoginController.java
+++ UserMicroservice/src/main/java/rev/controller/LoginController.java
@@ -1,11 +1,41 @@
 package rev.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import rev.model.User;
+import rev.service.UserService;
 
 @RestController
 @RequestMapping("/login-service")
 public class LoginController {
+	
+	private UserService userServ;
+	
+	@Autowired
+	public LoginController(UserService userServ) {
+		super();
+		this.userServ = userServ;
+	}
+	
+	@PostMapping(value="/login")
+	public @ResponseBody User login(@RequestBody User user){
+
+		User loguser = userServ.findByUsernameAndPassword(user.getUsername(),user.getPassword());
+		if (loguser == null) {
+			loguser = new User();
+			loguser.setUserId(-1);
+		}	
+		return loguser;
+
+	}
+	
 
 }
 
--- UserMicroservice/src/main/java/rev/dao/UserDao.java
+++ UserMicroservice/src/main/java/rev/dao/UserDao.java
@@ -4,10 +4,11 @@ package rev.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import rev.model.User;
 
-
+@Repository
 public interface UserDao extends JpaRepository<User, Integer>{
 	
 	public List<User> findAll();
