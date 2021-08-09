7bf4e98178f885e392df4b191b6e1d79f0783f0b
myibrahim36 <68923981+myibrahim36@users.noreply.github.com>
2021-08-09 15:05:42 -0400

    testing all user services

--- UserMicroservice/src/test/java/org/service/UserServiceImplTest.java
+++ UserMicroservice/src/test/java/org/service/UserServiceImplTest.java
@@ -5,6 +5,9 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -50,5 +53,89 @@ class UserServiceImplTest {
 		verify(userDao, times(1)).save(initialUser);
 		assertEquals(expectedUser, actualUser);
 	}
+	
+	@Test
+	void getAllUsers() {
+		// ARRANGE
+		List<User> initialList = new ArrayList<>();
+		initialList.add(new User("C-137", "Rick", "Sanchez", "pass","rickest@rick.com"));
+		initialList.add(new User("C-592", "Rick-Slow", "Sanchez", "pass","slowRick@councilOfRicks.com"));
+		initialList.add(new User("D-894", "Rick-Cocky", "Sanchez", "pass","cockyRick@councilOfRicks.com"));
+		
+		List<User> expectedList = new ArrayList<>();
+		expectedList.addAll(initialList);
+		
+		when(userDao.findAll()).thenReturn(initialList);
+		
+		// ACT
+		List<User> actualList = userServ.findAll();
+		
+		//ASSERT
+		verify(userDao, times(1)).findAll();
+		assertEquals(expectedList, actualList);
+	}
+	
+	@Test
+	void findByUsername() {
+		// ARRANGE
+		User initialUser = new User("It'sJake", "Jake",  "State-Farm", "pass",  "jake@statefarm.com");
+		User expectedUser = new User("It'sJake", "Jake",  "State-Farm", "pass", "jake@statefarm.com");
+		when(userDao.findByUsername(initialUser.getUsername())).thenReturn(initialUser);
+		
+		// ACT
+		User actualUser = userServ.findByUsername(initialUser.getUsername());
+		
+		// ASSERT
+		verify(userDao, times(1)).findByUsername(initialUser.getUsername());
+		assertEquals(expectedUser, actualUser);
+		
+	}
 
+	@Test
+	void findByUserId() {
+		// ARRANGE
+		User initialUser = new User("It'sJake", "Jake",  "State-Farm", "pass",  "jake@statefarm.com");
+		User expectedUser = new User("It'sJake", "Jake",  "State-Farm", "pass", "jake@statefarm.com");
+		when(userDao.findByUserId(initialUser.getUserId())).thenReturn(initialUser);
+		
+		// ACT
+		User actualUser = userServ.findByUserId(initialUser.getUserId());
+		
+		// ASSERT
+		verify(userDao, times(1)).findByUserId(initialUser.getUserId());
+		assertEquals(expectedUser, actualUser);
+		
+	}
+	
+	@Test
+	void findByUsernameAndPassword() {
+		// ARRANGE
+		User initialUser = new User("It'sJake", "Jake",  "State-Farm", "pass",  "jake@statefarm.com");
+		User expectedUser = new User("It'sJake", "Jake",  "State-Farm", "pass", "jake@statefarm.com");
+		when(userDao.findByUsernameAndPassword(initialUser.getUsername(), initialUser.getPassword())).thenReturn(initialUser);
+		
+		// ACT
+		User actualUser = userServ.findByUsernameAndPassword(initialUser.getUsername(), initialUser.getPassword());
+		
+		// ASSERT
+		verify(userDao, times(1)).findByUsernameAndPassword(initialUser.getUsername(), initialUser.getPassword());
+		assertEquals(expectedUser, actualUser);
+		
+	}
+	
+//	@Test
+//	void deleteUser() {
+////		fail("Not yet implemented");
+//		// ARRANGE
+//		User initialUser = new User(0, "It'sJake", "Jake", "From", "State-Farm", "pass", "pic", "bgnd-pic", "jake@statefarm.com");
+//		User expectedUser = new User(0, "It'sJake", "Jake", "From", "State-Farm", "pass", "pic", "bgnd-pic", "jake@statefarm.com");
+//		when(userDao.delete(initialUser)).thenReturn(initialUser);
+//		
+//		// ACT
+//		User actualUser = userServ.delete(initialUser);
+//		
+//		// ASSERT
+//		verify(userDao, times(1)).save(initialUser);
+//		assertEquals(expectedUser, actualUser);
+//	}
 }
