75e0a15e30b3aed6be4c9f07164720f9d906cdfc
myibrahim36 <68923981+myibrahim36@users.noreply.github.com>
2021-08-11 11:49:11 -0400

    added group table

--- UserMicroservice/src/main/java/rev/model/Group.java
+++ UserMicroservice/src/main/java/rev/model/Group.java
@@ -16,11 +16,14 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+
+@EqualsAndHashCode
 @Getter
 @Setter
 @NoArgsConstructor
